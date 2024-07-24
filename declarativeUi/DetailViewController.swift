7b2d0093c9cdc6eb4487c66f103cd863ce3437b2
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2024-07-24 11:43:58 +0700

    fix add navigation

--- declarativeUi/DetailViewController.swift
+++ declarativeUi/DetailViewController.swift
@@ -11,19 +11,53 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        let textLabel = UILabel()
+        textLabel.text = "Welcome to Detail"
+        textLabel.textColor = .black
+        textLabel.textAlignment = .center
+//        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Menentukan ukuran dan posisi UILabel
+        textLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        textLabel.center = view.center
+        
+        // Menambahkan UILabel ke view
+        view.addSubview(textLabel)
+        
+        // Mengatur constraints untuk UILabel agar berada di tengah layar [cara 2]
+//        NSLayoutConstraint.activate([
+//            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        ])
+        let iconName = "chevron.backward"
+        if let img = UIImage(systemName: iconName) {
+            let imgWidth = img.size.width
+            let imgHeight = img.size.height
+            
+            let button = UIButton(
+                frame: CGRect(x: 0, y: 0, width: imgWidth, height: imgHeight)
+            )
+            button.setBackgroundImage(img, for: .normal)
+            button.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+            
+            let backButton = UIBarButtonItem(customView: button)
+            self.navigationItem.leftBarButtonItem = backButton
+        } else {
+            print("Image '\(iconName)' not found in assets.")
+            let backButton = UIBarButtonItem(
+                title: "<",
+                style: .done,
+                target: self,
+                action: #selector(backAction)
+            )
+            self.navigationItem.leftBarButtonItem = backButton
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func backAction() {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
 
 }
